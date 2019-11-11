b36e2aefe4b61a550bab994beaf950f7c3bada6d
ratherlongname <shivam.bansal@live.com>
2019-11-11 07:34:09 +0530

    Start mqtt

--- server.py
+++ server.py
@@ -1,4 +1,17 @@
+def server_message_handler(client, userdata, msg):
+    # TODO
+    uid = msg.topic.split('/')[2]
+    message = str(msg.payload)
+    print("UID: {}, Message: {}".format(uid, message))
+    return
+
+def start_server():
+    import mqtt_server
+    mqtt_server.start_server(server_message_handler)
+    return
+
 def run_server():
+    start_server()
     return
 
 if __name__ == "__main__":
