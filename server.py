8cb7c03c404001ee44c066ba49c6d77f1e102138
ratherlongname <shivam.bansal@live.com>
2019-11-13 00:38:16 +0530

    Complete server.py

--- server.py
+++ server.py
@@ -2,6 +2,76 @@ def message_to_coordinates(message):
     # TODO
     return (0, 0)
 
+def get_direction(curr_loc, next_loc):
+    x_curr, y_curr = curr_loc
+    x_next, y_next = next_loc
+    if x_next is x_curr + 1 and y_next is y_curr:
+        return "R"
+    elif x_next is x_curr - 1 and y_next is y_curr:
+        return "L"
+    elif x_next is x_curr and y_next is y_curr + 1:
+        return "B"
+    elif x_next is x_curr and y_next is y_curr - 1:
+        return "F"
+    else:
+        raise AssertionError("next_loc and curr_loc are not adjacent")
+
+def send_next_command(uid, curr_loc):
+    import mqtt_server
+    import time
+    import routing
+    graph.remove_barrier(all_rpis[uid]['loc'])
+    graph.add_barrier(curr_loc)
+    all_rpis[uid]['loc'] = curr_loc
+    if all_rpis[uid]['pick'] is not (-1, -1):
+        if curr_loc is all_rpis[uid]['pick']:
+            # at pick loc
+            # send msg to pickup
+            # start going to drop
+            all_rpis[uid]['pick'] = (-1, -1)
+
+            mqtt_server.send_message("UP", uid)
+            time.sleep(5)
+            all_rpis[uid]['stepper'] = "up"
+
+            route, _ = routing.AStarSearch(curr_loc, all_rpis[uid]['drop'], graph)
+            next_loc = route[1]
+            graph.add_barrier(next_loc)
+            command = get_direction(curr_loc, next_loc)
+            mqtt_server.send_message(command, uid)
+
+        else:
+            # go to pickup
+            route, _ = routing.AStarSearch(curr_loc, all_rpis[uid]['pick'], graph)
+            next_loc = route[1]
+            graph.add_barrier(next_loc)
+            command = get_direction(curr_loc, next_loc)
+            mqtt_server.send_message(command, uid)
+
+    elif all_rpis[uid]['drop'] is not (-1, -1):
+        if curr_loc is all_rpis[uid]['drop']:
+            # at drop loc
+            # send msg to drop
+            all_rpis[uid]['drop'] = (-1, -1)
+
+            mqtt_server.send_message("DOWN", uid)
+            time.sleep(5)
+            all_rpis[uid]['stepper'] = "down"
+
+            if all_tasks:
+                locs = all_tasks.pop(0)
+                all_rpis[uid]['pick'] = locs[0]
+                all_rpis[uid]['drop'] = locs[1]
+                send_next_command(uid, curr_loc)
+
+        else:
+            # go to drop
+            route, _ = routing.AStarSearch(curr_loc, all_rpis[uid]['drop'], graph)
+            next_loc = route[1]
+            graph.add_barrier(next_loc)
+            command = get_direction(curr_loc, next_loc)
+            mqtt_server.send_message(command, uid)
+
 def server_message_handler(client, userdata, msg):
     # TODO
     message = str(msg.payload)
@@ -18,29 +88,13 @@ def server_message_handler(client, userdata, msg):
                             'pick':(-1, -1),
                             'drop':(-1, -1),
                             'stepper':'down'}
+            if all_tasks:
+                locs = all_tasks.pop(0)
+                all_rpis[uid]['pick'] = locs[0]
+                all_rpis[uid]['drop'] = locs[1]
+                send_next_command(uid, curr_loc)
         else:
-            graph.remove_barrier(all_rpis[uid]['loc'])
-            graph.add_barrier(curr_loc)
-            all_rpis[uid]['loc'] = curr_loc
-            if all_rpis[uid]['pick'] is not (-1, -1):
-                if curr_loc is all_rpis[uid]['pick']:
-                    # at pick loc
-                    # send msg to pickup
-                    # start going to drop
-                    all_rpis[uid]['pick'] = (-1, -1)
-                    
-                    import mqtt_server
-                    mqtt_server.send_message("up", uid)
-                    time.sleep(3)
-                    all_rpis[uid]['stepper'] = "up"
-
-                    import routing
-                    route, cost = routing.AStarSearch(curr_loc, all_rpis[uid]['drop'], graph)
-                    
-                else:
-                    # go to pickup
-            elif all_rpis[uid]['drop'] is not (-1, -1):
-                # go to drop
+            send_next_command(uid, curr_loc)
     return
 
 def start_server():
@@ -55,11 +109,67 @@ def read_warehouse_map():
     graph = routing.AStarGraph(map_filename)
     return
 
+def menu():
+    import routing
+    main_menu = ['q to quit',
+                'p to print map',
+                'g to take stuff from a to b',
+                'a to add barrier',
+                'r to remove barrier',
+                't to print current task queue']
+    while True:
+        print("\tWAREHOUSE SOFTWARE MENU")
+        for option in main_menu:
+            print(option)
+
+        choice = input()
+        if choice == 'q':
+            return
+        elif choice == 'p':
+            graph.print_map()
+        elif choice == 'g':
+            print("Enter pickup point (x,y):")
+            pick_loc = tuple([int(x) for x in input().split(',')])
+            print("Enter drop point (x,y):")
+            drop_loc = tuple([int(x) for x in input().split(',')])
+            result, _ = routing.AStarSearch(pick_loc, drop_loc, graph)
+            print ("Route: {}".format(result))
+            routing.draw_route(graph, result)
+            is_allocated = False
+            for uid in all_rpis:
+                if all_rpis[uid]['pick'] is (-1, -1) and all_rpis[uid]['drop'] is (-1, -1):
+                    is_allocated = True
+                    all_rpis[uid]['pick'] = pick_loc
+                    all_rpis[uid]['drop'] = drop_loc
+                    break
+            if not is_allocated:
+                all_tasks.append([pick_loc, drop_loc])
+                print("No rpi free right now, task added to queue")
+        elif choice is 'a':
+            print("Enter barrier point to add (x,y):")
+            barrier = tuple([int(x) for x in input().split(',')])
+            graph.add_barrier(barrier)
+        elif choice is 'r':
+            print("Enter barrier point to remove (x,y):")
+            barrier = tuple([int(x) for x in input().split(',')])
+            graph.remove_barrier(barrier)
+        elif choice is 't':
+            if all_tasks:
+                print("S.No.\tPick\tDrop")
+                for i, locs in enumerate(all_tasks):
+                    print("{}.\t{}\t{}".format(i+1, locs[0], locs[1]))
+            else:
+                print("Task queue empty...")
+    return
+
 def run_server():
     read_warehouse_map()
     global all_rpis
+    global all_tasks
+    all_tasks = []
     all_rpis = {}
     start_server()
+    menu()
     return
 
 if __name__ == "__main__":
